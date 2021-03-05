46c2addeee73db609d140e30fe2050e43200ad77
vijaybarnadmindtreecom <vijay.barnad@tmp.com>
2021-03-05 19:22:51 +0530

    updated

--- target/test-classes/PostUserRegistration.java
+++ target/test-classes/PostUserRegistration.java
@@ -1,47 +1,47 @@
-package com.serenitybddrestapi.testcases;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.serenitybddrestapi.model.User;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
-
-@RunWith(SerenityRunner.class)
-public class PostUserRegistration {
-	
-	static String firstName = "RestAPI";
-	static String lastName = "SerenityBDD";	
-	static String email = "restapi@mailinator.com";
-	
-	@BeforeClass
-	public static void init() {
-		RestAssured.baseURI = "https://reqres.in/api/users";
-	}
-	
-	@Test
-	public void registerNewMember() {
-		
-		User user = new User();
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		user.setEmail(email);
-		
-		
-		SerenityRest.rest().given()
-		.contentType(ContentType.JSON)
-		.log()
-		.all()
-		.when()
-		.body(user)
-		.post("/users")
-		.then()
-		.log()
-		.all()
-		.statusCode(201);
-	}
-}
+//package com.serenitybddrestapi.testcases;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import com.serenitybddrestapi.model.User;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.http.ContentType;
+//import net.serenitybdd.junit.runners.SerenityRunner;
+//import net.serenitybdd.rest.SerenityRest;
+//
+//@RunWith(SerenityRunner.class)
+//public class PostUserRegistration {
+//	
+//	static String firstName = "RestAPI";
+//	static String lastName = "SerenityBDD";	
+//	static String email = "restapi@mailinator.com";
+//	
+//	@BeforeClass
+//	public static void init() {
+//		RestAssured.baseURI = "https://reqres.in/api/users";
+//	}
+//	
+//	@Test
+//	public void registerNewMember() {
+//		
+//		User user = new User();
+//		user.setFirstName(firstName);
+//		user.setLastName(lastName);
+//		user.setEmail(email);
+//		
+//		
+//		SerenityRest.rest().given()
+//		.contentType(ContentType.JSON)
+//		.log()
+//		.all()
+//		.when()
+//		.body(user)
+//		.post("/users")
+//		.then()
+//		.log()
+//		.all()
+//		.statusCode(201);
+//	}
+//}
--- target/test-classes/PostUserRegistration2.java
+++ target/test-classes/PostUserRegistration2.java
@@ -1,51 +1,51 @@
-package com.serenitybddrestapi.testcases;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.serenitybddrestapi.model.User2;
-
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
-
-@RunWith(SerenityRunner.class)
-public class PostUserRegistration2 {
-	
-	static String firstName = "RestAPI";
-	static String lastName = "SerenityBDD";
-	static String userName= "RestAPISerenityBDD";
-	static String email = "restapi@mailinator.com";
-	static String password= "mindtree_01";
-	
-	@BeforeClass
-	public static void init() {
-		RestAssured.baseURI = "http://restapi.demoqa.com/";
-	}
-	
-	@Test
-	public void registerNewMember() {
-		
-		User2 user = new User2();
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		user.setEmail(email);
-		user.setPassword(password);
-		user.setUserName(userName);
-		
-		
-		SerenityRest.rest().given()
-		.contentType(ContentType.JSON)
-		.log()
-		.all()
-		.when()
-		.body(user)
-		.post("/customer/register")
-		.then()
-		.log()
-		.all()
-		.statusCode(201);
-	}
-}
+//package com.serenitybddrestapi.testcases;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import com.serenitybddrestapi.model.User2;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.http.ContentType;
+//import net.serenitybdd.junit.runners.SerenityRunner;
+//import net.serenitybdd.rest.SerenityRest;
+//
+//@RunWith(SerenityRunner.class)
+//public class PostUserRegistration2 {
+//	
+//	static String firstName = "RestAPI";
+//	static String lastName = "SerenityBDD";
+//	static String userName= "RestAPISerenityBDD";
+//	static String email = "restapi@mailinator.com";
+//	static String password= "mindtree_01";
+//	
+//	@BeforeClass
+//	public static void init() {
+//		RestAssured.baseURI = "http://restapi.demoqa.com/";
+//	}
+//	
+//	@Test
+//	public void registerNewMember() {
+//		
+//		User2 user = new User2();
+//		user.setFirstName(firstName);
+//		user.setLastName(lastName);
+//		user.setEmail(email);
+//		user.setPassword(password);
+//		user.setUserName(userName);
+//		
+//		
+//		SerenityRest.rest().given()
+//		.contentType(ContentType.JSON)
+//		.log()
+//		.all()
+//		.when()
+//		.body(user)
+//		.post("/customer/register")
+//		.then()
+//		.log()
+//		.all()
+//		.statusCode(201);
+//	}
+//}
